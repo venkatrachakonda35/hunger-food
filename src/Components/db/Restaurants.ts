8d39233227987a9ac4d538b88b806a8935662a37
venkatrachakonda <venkatrachakonda35@gmail.com>
2025-12-07 01:26:50 +0530

    simple

---
+++ src/Components/db/Restaurants.ts
@@ -0,0 +1,244 @@
+export const RESTAURANTS = [
+    {
+        "restaurantId": 1,
+        "restaurantName": "Box-8 Desi Meals",
+        "rating": 4.2,
+        "menuTypes": ["Soups", "Starters", "Biryani","Curries","Platter"],
+        "imageUrl": "https://tb-static.uber.com/prod/image-proc/processed_images/f16f68d8d1f9dd898911d06d68587008/3ac2b39ad528f8c8c5dc77c59abb683d.jpeg",
+        "location": "Saroor Nagar West"
+    },
+	{
+        "restaurantId": 2,
+        "restaurantName": "Haldiram's",
+        "rating": 4.1,
+        "menuTypes": ["Quick Bites", "Sweets"],
+        "imageUrl": "https://t4.ftcdn.net/jpg/02/54/92/05/360_F_254920579_xOIyVqIWxgUi0fSQ8FwBC95YlIVZpuCd.jpg",
+        "location": "Attapur"
+	},
+    {
+        "restaurantId": 3,
+        "restaurantName": "Cream Stone Ice Cream",
+        "rating": 4.0,
+        "menuTypes": ["Chocolate Wonders", "Natural Fruit", "Seasonal Star"],
+        "imageUrl": "https://b.zmtcdn.com/data/dish_photos/47b/f41d354763fdba2724436c7b4dd8647b.jpg",
+        "location": "Tolichowki"
+    },
+    {
+        "restaurantId": 4,
+        "restaurantName": "Ohri's Ming's Court",
+        "rating": 3.8,
+        "menuTypes": ["Dimsums ", "Super Saver Box", "Main Course"],
+        "imageUrl": "https://naturallynidhi.com/wp-content/uploads/2019/08/InstantPotSaffronRoseBiryani_Cover.jpg",
+        "location": "Banjara Hills"
+    },
+    {
+        "restaurantId": 5,
+        "restaurantName": "Nitro Grill",
+        "rating": 4.7,
+        "menuTypes": ["Flamed Nitro Grill Chicken", "Burgers", "Healthy Grilled Plates","Prime Beef Steaks","Sides"],
+        "imageUrl": "https://b.zmtcdn.com/data/pictures/chains/1/18854381/24ade015952bbfaeda9710b15cf39936.jpg",
+        "location": "Begumpet"
+    },
+    {
+        "restaurantId": 6,
+        "restaurantName": "Santosh Dhaba",
+        "rating": 3.5,
+        "menuTypes": ["Starters", "Biryani", "Rice","Curries"],
+        "imageUrl": "https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/sites/2/2015/10/Maa-da-dhaba.jpg",
+        "location": "Attapur"
+    },
+    {
+        "restaurantId": 7,
+        "restaurantName": "Mandi King Arabian Restaurant",
+        "rating": 4.8,
+        "menuTypes": ["Platter", "Biryani", "Dessert"],
+        "imageUrl": "https://serafinadubai.com/wp-content/uploads/2023/03/Mandi-Restaurants-1-1024x675.jpg.webp",
+        "location": "Saidabad"
+    },
+    {
+        "restaurantId": 8,
+        "restaurantName": "McDonald's",
+        "rating": 4.1,
+        "menuTypes": ["Burgers & Wraps", "Fried Chicken & Wings", "Coffee & Beverages (Hot and Cold)"],
+        "imageUrl": "https://d48-a.sdn.cz/d_48/c_img_E_F/9AsDph.jpeg?fl=res,600,,1,ffffff",
+        "location": "Humayun Nagar"
+    },
+    {
+        "restaurantId": 9,
+        "restaurantName": "Kritunga Multi-Cusine Restaurant",
+        "rating": 3.6,
+        "menuTypes": ["Soups", "Khichdi", "Dessert","Beverages"],
+        "imageUrl": "https://as2.ftcdn.net/v2/jpg/04/21/88/65/1000_F_421886584_E9JvnRItlKlU5OmB1P78sxdznEaZj1dn.jpg",
+        "location": "Lakdi-Ka-Pul"
+    },
+    {
+        "restaurantId": 10,
+        "restaurantName": "Karachi Bakery",
+        "rating": 4.3,
+        "menuTypes": ["Cakes", "Biscuit", "Appetizer"],
+        "imageUrl": "https://static.toiimg.com/photo/48201473.cms",
+        "location": "Banjara Hills"
+    },
+    {
+        "restaurantId": 11,
+        "restaurantName": "Fruit Bite Natural Juices & Salads",
+        "rating": 4.5,
+        "menuTypes": ["Natural Juices", "Fruit bowl"],
+        "imageUrl": "https://d2jx2rerrg6sh3.cloudfront.net/image-handler/picture/2018/4/shutterstock_1By_stockcreations.jpg",
+        "location": "Ameerpet"
+    },
+    {
+        "restaurantId": 12,
+        "restaurantName": "Bowl O' China",
+        "rating":4.1,
+        "menuTypes": ["Soup", "Starters", "Thukpa","Main Course"],
+        "imageUrl": "https://images.immediate.co.uk/production/volatile/sites/2/2022/09/chicken-burrito-bowl-2d56799.jpg?quality=90&resize=700,466",
+        "location": "Begumpet"
+    },
+    {
+        "restaurantId": 13,
+        "restaurantName": "Hotel Niagara",
+        "rating": 3.9,
+        "menuTypes": ["Soups", "Starters", "Biryani","Salad"],
+        "imageUrl": "https://t4.ftcdn.net/jpg/05/80/83/11/360_F_580831163_gHJgKLXz4TxnZRSnVpdi779YnI6Vk0FL.jpg",
+        "location": "Chaderghat"
+    },
+    {
+        "restaurantId": 14,
+        "restaurantName": "Chutneys",
+        "rating": 4.2,
+        "menuTypes": ["Dosa", "Veg Main Course", "Jain Specialities","Rice & Indian Breads"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/hsmuwtvvgj30r0mnldee",
+        "location": "Sri Sai Tarun Enclave"
+    },
+    {
+        "restaurantId": 15,
+        "restaurantName": "Mandi @36",
+        "rating": 4.1,
+        "menuTypes": ["Soups", "Mandi", "Platter", "Dessert"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/b05aeee423432612309044c49b111156",
+        "location": "Banjara Hills"
+    },
+    {
+        "restaurantId": 16,
+        "restaurantName": "Chinese Wok",
+        "rating": 4.0,
+        "menuTypes": ["Momos", "Rice & Noodles - Half", "Gravy"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e0839ff574213e6f35b3899ebf1fc597",
+        "location": "Ameerpet"
+    },
+    {
+        "restaurantId": 17,
+        "restaurantName": "Paradise Biryani",
+        "rating": 3.7,
+        "menuTypes": ["Soups", "Starters", "Biryani","curries","Dessert"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/dv8ste4rk78q2ykiuzh9",
+        "location": "Khairtabad"
+        
+    },
+    {
+        "restaurantId": 18,
+        "restaurantName": "Pista House Bakery",
+        "rating": 4.3,
+        "menuTypes": ["Roll", "Wrap", "Cake", "Salad"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/xrnrtevdf9w3pfvxlaei",
+        "location": "Mehdipatnam"
+    },
+    {
+        "restaurantId": 19,
+        "restaurantName": "Makers Of Milkshakes",
+        "rating": 4.1,
+        "menuTypes": ["Million Dollar Shakes", "Fruit Walker", "Coco Bros", "Mom Special Shakes"],
+        "imageUrl": "https://www.tastingtable.com/img/gallery/13-fast-food-milkshakes-ranked-worst-to-best/intro-1690826763.jpg",
+        "location": "Padmarao Nagar"
+    },
+    {
+        "restaurantId": 20,
+        "restaurantName": "Balaji Family Dhaba",
+        "rating": 3.8,
+        "menuTypes": ["Pulao [Basmati Ki Khusbu]", "Biryani [Basmati Ki Khushbu]", "Jumbo [Family Pack]","Paneer & Kaju Curries","Rolls"],
+        "imageUrl": "https://indiagateflours.com/superadmin/upload/blog/punjabi-recipes.jpg",
+        "location": "New Nallakunta"
+    },
+    {
+        "restaurantId": 21,
+        "restaurantName": "Hotel Sohail Waves",
+        "rating": 4.2,
+        "menuTypes": ["Starters", "Biryani", "Khichdi", "Dessert"],
+        "imageUrl": "https://images.lifestyleasia.com/wp-content/uploads/sites/7/2022/04/21152009/shutterstock_1183813381-1.jpg",
+        "location": "Chanchalguda"
+    },
+    {
+        "restaurantId": 22,
+        "restaurantName": "Ameerpet Bawarchi",
+        "rating": 4.3,
+        "menuTypes": ["Salad", "Khichdi", "Biryani", "Beverages"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/727ee82703c6fa71d1c954ead91dc20d",
+        "location": "Attapur"
+    },
+    {
+        "restaurantId": 23,
+        "restaurantName": "Oven Story Pizza - Standout Toppings",
+        "rating": 4.0,
+        "menuTypes": ["Pizzas", "Pasta"],
+        "imageUrl": "https://www.mumbailive.com/images/news/images_1517830413799_Pizza%20cover%20final.jpg?fm=webp&w=1368",
+        "location": "Abids"
+    },
+    {
+        "restaurantId": 24,
+        "restaurantName": "Behrouz Biryani",
+        "rating": 4.1,
+        "menuTypes": ["Starters", "Biryani", "Dessert"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/89fccaa76f2f760e2742b9e53d32bb69",
+        "location": "Abids"
+    },
+    {
+        "restaurantId": 25,
+        "restaurantName": "Burger king",
+        "rating": 4.1,
+        "menuTypes": ["Burgers", "Beverages & Desserts"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf",
+        "location": "Himayath Nagar"
+    },
+    {
+        "restaurantId": 26,
+        "restaurantName": "Peshawar Restaurant",
+        "rating": 3.4,
+        "menuTypes": ["Starters", "Biryani", "Platter","Dessert"],
+        "imageUrl": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/vb0uuysg8hzei8r570v9",
+        "location": "Attapur"
+    },
+    {
+        "restaurantId": 27,
+        "restaurantName": "Sri Balaji Darshan",
+        "rating": 4.0,
+        "menuTypes": ["Idli, Vada & Mysore Bajji", "Special Dosas", "Classic Tiffin"],
+        "imageUrl": "https://lh3.googleusercontent.com/p/AF1QipMx5Zjye_WOTyaayQg4a3-5bXfOQMQxRHHZ-kIE=w1080-h608-p-no-v0",
+        "location": "Nallakunta & Vidyanagar"
+    },
+    {
+        "restaurantId": 28,
+        "restaurantName": "Noodle King",
+        "rating": 3.7,
+        "menuTypes": ["Noodles", "Manchurian", "Veg-Non-Veg Combo","Spring Roll","Platters"],
+        "imageUrl": "https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/hakka-noodles-recipe.jpg",
+        "location": "Ameerpet"
+    },
+    {
+        "restaurantId": 29,
+        "restaurantName": "Udupi Park Pure Veg",
+        "rating": 4.4,
+        "menuTypes": ["Idlis and Vada", "Dosa", "Classic Tiffin"],
+        "imageUrl": "https://t3.ftcdn.net/jpg/05/33/82/34/360_F_533823407_h0wVzQub7h3b6OZVWE44BPf5E6SHndxI.jpg",
+        "location": "Ameerpet"
+    },
+    {
+        "restaurantId": 30,
+        "restaurantName": "Pizza Hut",
+        "rating": 3.8,
+        "menuTypes": ["Pizzas", "Pasta", "Beverages & Desserts"],
+        "imageUrl": "https://wallpapers.com/images/featured/pizza-hut-gqkamsjea7s76iqm.jpg",
+        "location": "Mehdipatnam"
+    }
+	
+]
